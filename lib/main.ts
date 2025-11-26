c58ff2e27f2518a03f12a993da240fc4d49f3853
diba1013 <diba1013@web.de>
2025-11-26 19:24:42 +0100

    feat: add some more output to trigger pipeline
    Signed-off-by: diba1013 <diba1013@web.de>

--- lib/main.ts
+++ lib/main.ts
@@ -3,6 +3,7 @@
 export type MainOutput = {
 	name: string;
 	version: string;
+	description: string;
 	path: string;
 };
 
@@ -10,6 +11,7 @@ export function prepare(): MainOutput {
 	return {
 		name: process.env.__NAME__ ?? "unknown",
 		version: process.env.__VERSION__ ?? "0.0.0",
+		description: "A basic test for a random package",
 		path: import.meta.dirname,
 	};
 }
